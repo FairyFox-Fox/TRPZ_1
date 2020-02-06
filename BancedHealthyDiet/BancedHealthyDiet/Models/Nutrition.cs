83b1b777ec6cabc5f4aa5db546cf4fbc691eb530
FairyFox-Fox <tyschenk40@gmail.com>
2020-02-06 22:32:24 +0200

    Added Nutrition model

---
+++ BancedHealthyDiet/BancedHealthyDiet/Models/Nutrition.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace BancedHealthyDiet.Models
+{
+    public class Nutrition
+    {
+        private Guid id;
+        public Guid Id
+        {
+            get
+            {
+                if (id == Guid.Empty)
+                    return id = Guid.NewGuid();
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        private double proteins;
+        public double Proteins
+        {
+            get => proteins;
+            set
+            {
+                if (proteins < 0)
+                    throw new ArgumentOutOfRangeException("Proteins");
+                proteins = value;
+            }
+        }
+
+        private double carbonhydrates;
+        public double Carbonhydrates
+        {
+            get => carbonhydrates;
+            set
+            {
+                if (carbonhydrates < 0)
+                    throw new ArgumentOutOfRangeException("Carbonhydrates");
+                carbonhydrates = value;
+            }
+        }
+
+        private double fats;
+        public double Fats
+        {
+            get => fats;
+            set
+            {
+                if (fats < 0)
+                    throw new ArgumentOutOfRangeException("Fats");
+                fats = value;
+            }
+        }
+
+        private double vitamins;
+        public double Vitamins
+        {
+            get => vitamins;
+            set
+            {
+                if (vitamins < 0)
+                    throw new ArgumentOutOfRangeException("Vitamins");
+                vitamins = value;
+            }
+        }
+
+        private double minerals;
+        public double Minerals
+        {
+            get => minerals;
+            set
+            {
+                if (minerals < 0)
+                    throw new ArgumentOutOfRangeException("Minerals");
+                minerals = value;
+            }
+        }
+
+        private double calories;
+        public double Calories
+        {
+            get => calories;
+            set
+            {
+                if (calories < 0)
+                    throw new ArgumentOutOfRangeException("Calories");
+                calories = value;
+            }
+        }
+
+    }
+}
---
+++ BancedHealthyDiet/BancedHealthyDiet/Models/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancedHealthyDiet.Models
+{
+    public class Product
+    {
+        private Guid id;
+        public Guid Id
+        {
+            get
+            {
+                if (id == Guid.Empty)
+                   return id = Guid.NewGuid();
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        private string productName;
+        public string ProductName
+        {
+            get => productName;
+            set
+            {
+                if (String.IsNullOrEmpty(productName))
+                    throw new ArgumentException("Product name is not valid");
+                productName = value;
+            }
+        }
+
+        private Nutrition nutrition; 
+        public Nutrition Nutrition
+        {
+            get => nutrition;
+            set
+            {
+                if (value != null)
+                    nutrition = value;
+                throw new ArgumentNullException("Nutrition");
+            }
+        }
+        public Product(string name)
+        {
+            this.productName = name;
+        }
+    }
+}
