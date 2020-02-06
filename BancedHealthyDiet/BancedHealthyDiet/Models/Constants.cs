ec090866806196536a088b9ba0ae6edfa7415486
FairyFox-Fox <tyschenk40@gmail.com>
2020-02-06 22:52:16 +0200

    Added Ingredient model

---
+++ BancedHealthyDiet/BancedHealthyDiet/Models/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancedHealthyDiet.Models
+{
+    public  static class Constants
+    {
+        private static readonly List<string> measurements = new List<String> { "г.", "кг.", "л.", "стак. (200 мл)", "мл", "чайн. л.", "стол. л.", "по вкусу" };
+       
+    }
+}
---
+++ BancedHealthyDiet/BancedHealthyDiet/Models/Ingredient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancedHealthyDiet.Models
+{
+    public class Ingredient
+    {
+        private Guid id;
+        private Product product;
+        public Product Product
+        {
+            get => product;
+            set
+            {
+                if(product==null)
+                    throw new ArgumentNullException("Product");
+                product = value;
+            }
+        }
+        private double weight;
+        public double Weight
+        {
+            get => weight;
+            set
+            {
+                if (weight < 0)
+                    throw new ArgumentOutOfRangeException("Weight");
+                weight = value;
+            }
+        }
+
+        private string measurementUnit;
+        public string MeasurementUnit
+        {
+            get => measurementUnit;
+            set
+            {
+                if (String.IsNullOrEmpty(measurementUnit))
+                    throw new ArgumentException("MeasurementUnit name is not valid");
+                measurementUnit = value;
+            }
+        }
+    }
+}
