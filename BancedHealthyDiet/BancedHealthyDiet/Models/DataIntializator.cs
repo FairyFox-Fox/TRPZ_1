0151f529acb93c6977369f7065e9d4ae722d0858
FairyFox-Fox <tyschenk40@gmail.com>
2020-02-06 23:09:56 +0200

    Adde DataSet with initializator

---
+++ BancedHealthyDiet/BancedHealthyDiet/Models/DataIntializator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BancedHealthyDiet.Models
+{
+    internal class DataIntializator
+    {
+        public DataIntializator()
+        {
+        }
+
+        internal void InitWithData(DataSet dataSet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BancedHealthyDiet/BancedHealthyDiet/Models/DataSet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancedHealthyDiet.Models
+{
+    internal  class DataSet
+    {
+        private List<Product> products;
+        public List<Product> Products
+        {
+            get => products;
+            set
+            {
+                if (value != null)
+                    products= value;
+            }
+        }
+        private List<Recipe> recipes;
+        public List<Recipe> Recipes
+        {
+            get => recipes;
+            set
+            {
+                if (value != null)
+                    recipes = value;
+            }
+        }
+        private static DataSet dataSetInstance;
+        private DataSet()
+        {
+            products = new List<Product>();
+            recipes = new List<Recipe>();
+            DataIntializator dataIntializator = new DataIntializator();
+            dataIntializator.InitWithData(this);
+
+        }
+        public static DataSet GetInsatnce()
+        {
+            if (dataSetInstance == null)
+                dataSetInstance = new DataSet();
+            return dataSetInstance;
+        }
+    }
+}
