38aa812d16891bd9bfec907a706dcc8e2e803644
FairyFox-Fox <tyschenk40@gmail.com>
2020-02-09 11:43:14 +0200

    MainWindow added command bindings

--- BancedHealthyDiet/BancedHealthyDiet/Converters/BoolToVisibility.cs
+++ BancedHealthyDiet/BancedHealthyDiet/Converters/BoolToVisibility.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
 
 namespace BancedHealthyDiet.Converters
 {
-    class BoolToVisibility
+    class BoolToVisibility : IValueConverter
     {
+        private object GetVisibility(object value)
+        {
+            if (!(value is bool))
+                return Visibility.Collapsed;
+            bool val = (bool)value;
+            if (!val)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return GetVisibility(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BancedHealthyDiet/BancedHealthyDiet/ViewModels/MainViewModel.cs
+++ BancedHealthyDiet/BancedHealthyDiet/ViewModels/MainViewModel.cs
@@ -11,26 +11,84 @@ using System.Windows.Input;
 namespace BancedHealthyDiet.ViewModels
 {
     public class MainViewModel:BaseViewModel
-    {
-        private Nutrition totalNutrition;
-        public Nutrition TotalNutrition
-        {
-            get => totalNutrition;
-            set
-            {
-                totalNutrition = value;
-                OnPropertyChanged(nameof(TotalNutrition));
-            }
-        }
-        private void CalculateTotalNutrition(List<Recipe> recipes)
+    { 
+    //{
+    //    private Nutrition totalNutrition;
+    //    public Nutrition TotalNutrition
+    //    {
+    //        get => totalNutrition;
+    //        set
+    //        {
+    //            totalNutrition = value;
+    //            OnPropertyChanged(nameof(TotalNutrition));
+    //        }
+    //    }
+        //private void CalculateTotalNutrition(List<Recipe> recipes)
+        //{
+        //    var totalNutrition = new Nutrition();
+        //    foreach(var recipe in recipes)
+        //    {
+        //        foreach( var ingredient in recipe.Ingredients)
+        //        {
+        //            totalNutrition = totalNutrition + ingredient.Product.Nutrition;
+        //        }
+        //    }
+        //}
+
+        private void CalculateTotalNutrition(Recipe recipe)
         {
             var totalNutrition = new Nutrition();
-            foreach(var recipe in recipes)
+            foreach (var ingredient in recipe.Ingredients)
             {
-                foreach( var ingredient in recipe.Ingredients)
-                {
-                    totalNutrition = totalNutrition + ingredient.Product.Nutrition;
-                }
+                CheckWeight(ingredient);
+                totalNutrition = CalculateNutrition(totalNutrition,ingredient.Product.Nutrition, ingredient.Weight);
+                
+            }
+            recipe.TotalNutrition = totalNutrition;
+
+        }
+        private Nutrition CalculateNutrition(Nutrition firstNutrition, Nutrition secondNutrition, double weight)
+        {
+            
+            return new Nutrition
+            {
+                Calories = firstNutrition.Calories/100*weight + secondNutrition.Calories / 100 * weight,
+                Minerals = firstNutrition.Minerals / 100 * weight + secondNutrition.Minerals / 100 * weight,
+                Proteins = firstNutrition.Proteins / 100 * weight + secondNutrition.Proteins / 100 * weight,
+                Fats = firstNutrition.Fats / 100 * weight + secondNutrition.Fats / 100 * weight,
+                Vitamins = firstNutrition.Vitamins / 100 * weight + secondNutrition.Vitamins / 100 * weight,
+                Carbonhydrates = firstNutrition.Carbonhydrates / 100 * weight + secondNutrition.Carbonhydrates / 100 * weight
+            };
+        }
+
+        private void CheckWeight(Ingredient ingredient)
+        {
+            switch (ingredient.MeasurementUnit)
+            {
+                case "gr":
+                    return;
+                case "kg":
+                    ingredient.Weight=ingredient.Weight*1000;
+                    break; 
+                case "l":
+                    ingredient.Weight = ingredient.Weight * 1000;
+                    break;
+                case "glasses (200 ml)":
+                    ingredient.Weight = ingredient.Weight * 200;
+                    break;
+                case "ml":
+                    return;
+                case "tsp":
+                    ingredient.Weight = ingredient.Weight * 5;
+                    break;
+                case "tbsp":
+                    ingredient.Weight = ingredient.Weight * 15;
+                    break;
+                case "on taste":
+                    ingredient.Weight = ingredient.Weight * 0;
+                    break;
+                default:
+                    return;
             }
         }
 
@@ -43,6 +101,8 @@ namespace BancedHealthyDiet.ViewModels
         {
             var dataSet = DataSet.GetInsatnce();
             this.recipesCollection = new ObservableCollection<Recipe>(dataSet.Recipes);
+            foreach (var recipe in recipesCollection)
+                CalculateTotalNutrition(recipe);
         }
         private Recipe selectedRecipe;
         private List<Recipe> selectedRecipes;
@@ -62,37 +122,59 @@ namespace BancedHealthyDiet.ViewModels
             set
             {
                 selectedRecipe = value;
+                
                 OnPropertyChanged(nameof(SelectedRecipe));
+
             }
         }
-        public void AddSelectedRecipe(Recipe selectedRecipe)
+
+        ICommand addRecipeToList;
+        public ICommand AddRecipeToList
+        {
+            get {
+                if (addRecipeToList == null)
+                    addRecipeToList = new RelayCommand(method => AddSelectedRecipe());
+                return addRecipeToList;
+            }
+        }
+        public void AddSelectedRecipe()
         {
             if(SelectedRecipe!=null)
             {
-                if(!selectedRecipes.Contains(selectedRecipe))
-                    selectedRecipes.Add(selectedRecipe);
+                if(!SelectedRecipes.Contains(selectedRecipe))
+                    SelectedRecipes.Add(selectedRecipe);
             }
         }
 
         ICommand showRecipeDeatilViewCommand;
-        private bool isDetailViewCollapsed;
+        private bool isDetailViewCollapsed=true;
         public bool IsDetailViewCollapsed
         {
             get => isDetailViewCollapsed;
+            set
+            {
+                isDetailViewCollapsed = value;
+                OnPropertyChanged(nameof(IsDetailViewCollapsed));
+            }
+            
         }
         public ICommand ShowRecipeDeatilViewCommand
         {
             get
             {
-                if (isDetailViewCollapsed == null)
+                if (showRecipeDeatilViewCommand == null)
                     showRecipeDeatilViewCommand = new RelayCommand(method=>ShowRecipeDeatilView());
+             
                 return showRecipeDeatilViewCommand;
+               
             }
         }
 
         private void ShowRecipeDeatilView()
         {
-            isDetailViewCollapsed = true;
+
+            IsDetailViewCollapsed = false;
+           
         }
     }
 }
