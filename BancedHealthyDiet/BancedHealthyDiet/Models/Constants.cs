4c3bb90de47f3e4aebf5b20fdedca0be43d51d79
FairyFox-Fox <tyschenk40@gmail.com>
2020-02-12 13:52:35 +0200

    Adde updated class diagram; File pahes in appconfig

--- BancedHealthyDiet/BancedHealthyDiet/Models/Constants.cs
+++ BancedHealthyDiet/BancedHealthyDiet/Models/Constants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace BancedHealthyDiet.Models
     public  static class Constants
     {
         public static readonly List<string> measurements = new List<String> { "gr", "kg", "l", "glasses (200 ml)", "ml", "tsp", "tbsp", "on taste" };
-        public const string FILE_PATH_TO_PRODUCTS = "D:\\DataSet\\Products";
-        public const string FILE_PATH_TO_RECIPES = "D:\\DataSet\\Recipes";
+        public static string FILE_PATH_TO_PRODUCTS = ConfigurationManager.AppSettings["PathToProducts"];
+        public static string FILE_PATH_TO_RECIPES = ConfigurationManager.AppSettings["PathToRecipess"];
     }
 }
